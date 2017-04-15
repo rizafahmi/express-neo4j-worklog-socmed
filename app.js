ce812cb528dbd971aab7dee82a97bc304419a36f
Riza Fahmi <rizafahmi@gmail.com>
2017-04-15 07:10:25 +0700

    Finish selecting template engine. Proceed

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ app.use('/', index)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  var err = new Error('Not Found')
+  const err = new Error('Not Found')
   err.status = 404
   next(err)
 })
@@ -67,7 +67,6 @@ app.use(function (err, req, res, next) {
   res.locals.message = err.message
   res.locals.error = req.app.get('env') === 'development' ? err : {}
 
-  console.log(err)
   // render the error page
   res.status(err.status || 500)
   res.render('error')
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express')
 var router = express.Router()
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/', (req, res, next) => {
   let user
   if (req.session) {
     user = req.session.user
@@ -10,6 +10,19 @@ router.get('/', function (req, res, next) {
     user = ''
   }
   res.render('index', { title: 'DevLog', user: user })
+})
+router.get('/register', (req, res, next) => {
+  res.render('register')
+})
+router.post('/register', (req, res, next) => {
+  res.send(`${req.body.username} ${req.body.password}`)
+})
+
+router.get('/login', (req, res, next) => {
+  res.render('login')
+})
+router.post('/register', (req, res, next) => {
+  res.send('TODO')
 })
 
 module.exports = router
